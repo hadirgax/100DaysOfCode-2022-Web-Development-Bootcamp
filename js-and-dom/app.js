2e40caeb937da44549154a97d8c320402ecf6c00
hadirga <halexandergc@gmail.com>
2022-03-04 16:10:12 -0300

    drill into the dom

--- js-and-dom/app.js
+++ js-and-dom/app.js
@@ -1,2 +1,8 @@
-console.dir(document);
-document.body.children[1].children[0].href = 'https://google.com';
+// console.dir(document);
+// document.body.children[1].children[0].href = 'https://google.com';
+
+// let a = document.querySelector('a');
+// a.href = 'https://google.com';
+
+console.log(document.body.firstChild);
+console.log(document.body.firstElementChild);
